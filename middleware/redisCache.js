1183947f8053c8c4c332ce5eda8e328d6b44b518
Tsinu Gezahgn <tsinu.gezahgn@n-ems.com>
2019-01-27 22:27:59 +0100

    minor fix

--- middleware/redisCache.js
+++ middleware/redisCache.js
@@ -1,7 +1,7 @@
 
 const redis = require('redis')
-const client = redis.createClient('6379','127.0.0.1');
-// const client = redis.createClient(process.env.REDIS_URI);
+// const client = redis.createClient('6379','127.0.0.1');
+const client = redis.createClient(process.env.REDIS_URI);
 
 
 const redisMiddleware = (req, res, next) => {
